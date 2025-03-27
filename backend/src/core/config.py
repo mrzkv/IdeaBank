0b4bed716c910cf135b07bc831a4798484cbd2fa
Timofey <Timofeygorev.steam@gmail.com>
2025-03-27 13:27:41 +0500

    Initialize backend framework structure with FastAPI
    Added essential modules for database handling, server configuration, and API routing. Included core dependencies in `requirements.txt` and provided a sample `.env` file for environment setup.

---
+++ backend/src/core/config.py
@@ -0,0 +1,58 @@
+from loguru import logger
+from authx import AuthXConfig
+from pydantic_settings import BaseSettings
+from dotenv import load_dotenv
+from datetime import timedelta
+import time
+import os
+
+
+class DataBaseSettings(BaseSettings):
+    url: str
+    pool_size: int
+    max_overflow: int
+
+class RoutersPrefix(BaseSettings):
+    auth: str
+
+class Settings(BaseSettings):
+    SERVER_START_TIME: int
+    SERVER_PORT: int = 8765
+    IP_ADDRESS: str = 'localhost'
+    db: DataBaseSettings
+    jwt: AuthXConfig
+    routers: RoutersPrefix
+
+logger.add(
+    sink=f'app.log',
+    level='DEBUG',
+    format='{time} {level} {message}',
+    serialize=False,
+    rotation='10 MB',
+    compression='zip'
+)
+
+load_dotenv()
+
+settings = Settings(
+    SERVER_START_TIME=int(time.time()),
+    SERVER_PORT=int(os.getenv('SERVER_PORT', 8765)),
+    IP_ADDRESS=os.getenv('IP_ADDRESS', '127.0.0.1'),
+    db=DataBaseSettings(
+        url=os.getenv('DB_CONNECTION_STRING'),
+        pool_size=int(os.getenv('DB_POOL_SIZE')),
+        max_overflow=int(os.getenv('DB_MAX_OVERFLOW'))
+    ),
+    routers=RoutersPrefix(
+        auth='/v1/api/auth'
+    ),
+    jwt=AuthXConfig(
+        JWT_ACCESS_TOKEN_EXPIRES=timedelta(minutes=30),
+        JWT_REFRESH_TOKEN_EXPIRES=timedelta(days=15),
+        JWT_CSRF_IN_COOKIES=False,
+        JWT_ALGORITHM='RS256',
+        JWT_PUBLIC_KEY=os.getenv('JWT_PUBLIC_KEY'),
+        JWT_SECRET_KEY=os.getenv('JWT_SECRET_KEY')
+    )
+
+)
---
+++ backend/src/main.py
@@ -0,0 +1,59 @@
+
+import uvicorn
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
+from backend.src.core.config import settings
+from backend.src.core.db_helper import db_helper
+
+import time
+from loguru import logger
+from contextlib import asynccontextmanager
+from typing import AsyncGenerator
+
+
+@asynccontextmanager
+async def lifespan(app: FastAPI) -> AsyncGenerator[None, None]:
+    logger.info("Application lifespan started")
+    try:
+        yield
+    except Exception as e:
+        logger.error(f'Error during application lifespan: {e}')
+    finally:
+        try:
+            logger.info(f"Server total uptime: {int(time.time() - settings.SERVER_START_TIME)}")
+            await db_helper.dispose()
+        except Exception as e:
+            logger.error(f'Error during application cleanup: {e}')
+
+
+
+
+
+
+
+main_app = FastAPI(lifespan=lifespan)
+
+main_app.add_middleware(
+    CORSMiddleware,
+    allow_origins=['*'],
+    allow_credentials=True,
+    allow_methods=['*'],
+    allow_headers=['*'],
+)
+
+@main_app.get('/ping')
+async def ping():
+    return 'pong'
+
+
+def start_server():
+    uvicorn.run(
+        host=settings.IP_ADDRESS,
+        port=settings.SERVER_PORT,
+        app='main:main_app',
+        log_level='debug'
+    )
+
+if __name__ == '__main__':
+    start_server()

05bd82e9501636bb640f137ad2a952deb2099759
Timofey <Timofeygorev.steam@gmail.com>
2025-03-27 14:21:10 +0500

    Adedd db tables

---
+++ backend/src/core/db_tables.py
@@ -0,0 +1,39 @@
+from sqlalchemy import Column, BigInteger, String, Text, TIMESTAMP
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+
+class UsersFio(Base):
+    __tablename__ = 'users_fio'
+
+    user_id = Column(BigInteger, primary_key=True, unique=True)
+    name = Column(String)
+    surname = Column(String)
+    patronymic = Column(String)
+
+
+class Ideas(Base):
+    __tablename__ = 'ideas'
+
+    id = Column(BigInteger, primary_key=True, autoincrement=True, unique=True)
+    name = Column(String)
+    description = Column(Text)
+    status = Column(String)
+    start_date = Column(TIMESTAMP)
+    end_date = Column(TIMESTAMP)
+    creator_id = Column(BigInteger)
+    expert_id = Column(BigInteger)
+    solution = Column(String)
+    solution_description = Column(Text)
+
+
+class Users(Base):
+    __tablename__ = 'users'
+
+    id = Column(BigInteger, primary_key=True, autoincrement=True, unique=True)
+    login = Column(String, unique=True)
+    hashed_password = Column(String)
+    role = Column(String)
+    join_date = Column(TIMESTAMP)
+    status = Column(String)
